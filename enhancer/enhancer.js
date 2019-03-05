bc20a72d4890a0e9c9982f4b20816f5d0fdb6753
Olivia Osborn <olioz95@gmail.com>
2019-03-04 19:14:34 -0500

    wrote a few more tests

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -1,15 +1,43 @@
-module.exports = {
-  success,
-  fail,
-  repair
+const enhancer = {
+  success(item) {
+    enhancedInt = parseInt(item.enhancement, 10);
+    return enhancedInt;
+  },
+
+  fail(item) {
+    if (item.enhancement < 15 && item.durability < 25) {
+      return { ...item };
+    }
+    const enhancement =
+      item.enhancement < 15 ? item.enhancement - 1 : item.enhancement;
+    const durability =
+      item.enhancement < 15 ? item.durability - 5 : item.durability;
+    return { ...item, durability, enhancement };
+  },
+
+  repair(item) {
+    return {
+      ...item,
+      durability: 100
+    };
+  }
 };
 
-function success(item) {}
+module.exports = {
+  enhancer
+};
 
-function fail(item) {}
+const item1 = {
+  name: "sword",
+  type: "weapon",
+  durability: 20,
+  enhancement: "+5"
+};
 
-function repair(item) {
-  const repairedItem = item;
-  repairedItem.durability = 100;
-  return repairedItem;
-}
+const levels = {
+  16: "PRI",
+  17: "DUO",
+  18: "TRI",
+  19: "TET",
+  20: "PEN"
+};
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,6 +1,35 @@
-const { success, fail, repair } = require("./enhancer");
+const { enhancer } = require("./enhancer");
 
-test(`sets durability to 100 correctly`, () => {
-  expect(repair({ durability: 50 }).durability).toBe(100);
-  expect(repair({ durability: 0 }).durability).toBe(100);
+describe("enhancer", () => {
+  describe("repair()", () => {
+    test(`sets durability to 100 correctly`, () => {
+      expect(enhancer.repair({ durability: 50 }).durability).toBe(100);
+      expect(enhancer.repair({ durability: 0 }).durability).toBe(100);
+    });
+
+    describe("fail()", () => {
+      it("durability decreases by 5 if enhancement is between 0 and 14", () => {
+        expect(
+          enhancer.fail({
+            enhancement: 14,
+            durability: 85
+          }).durability
+        ).toBe(80);
+      });
+      it("If enhancement in 14 or lower, the itme cannot be enhanced if the durability is below 25", () => {
+        const item = {
+          enhancement: 14,
+          durability: 24
+        };
+        expect(enhancer.fail(item)).toEqual(item);
+      });
+    });
+
+    describe("success()", () => {
+      test(`sets durability to 100 correctly`, () => {
+        expect(enhancer.success({ enhancement: "+8" })).toBe(8);
+        expect(enhancer.success({ enhancement: "+10" })).toBe(10);
+      });
+    });
+  });
 });
